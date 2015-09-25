34ca3279d80d9ae8830d439dec87268b50e2c64f
Chenyang Liu <chenyang.liu@bodylabs.com>
2015-09-25 17:31:19 -0400

    Restructure

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             "flask"
+description      "flask-poc"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+maintainer       "Bodylabs, Inc"
+license          "Apache 2.0"
+version          "1.0.0"
+
+depends 'python'
+depends 'supervisor'
