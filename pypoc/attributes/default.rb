bfa095f93f594c969b16d800ccbbaf07c7f7c704
Chenyang Liu <chenyang.liu@bodylabs.com>
2015-09-30 00:38:52 -0400

    This time should be fine

--- pypoc/attributes/default.rb
+++ pypoc/attributes/default.rb
@@ -1,4 +1,4 @@
 # Defaults attributes
 default['python']['version'] = '2.7.10'
 default['pypoc']['BUFFER_TIMEOUT'] = 1
-default['pypoc']['supervisor_log_dir'] = '/bodylabs/logs/'
+default['pypoc']['supervisor_log_dir'] = '/bodylabs/logs'
--- pypoc/recipes/setup.rb
+++ pypoc/recipes/setup.rb
@@ -8,5 +8,5 @@ end
 # create directory
 execute 'create log directory for supervisor' do
   not_if { ::Dir.exists?("#{node['pypoc']['supervisor_log_dir']}") }
-  command "sudo mkdir #{node['pypoc']['supervisor_log_dir']}"
+  command "sudo mkdir -p #{node['pypoc']['supervisor_log_dir']}"
 end
