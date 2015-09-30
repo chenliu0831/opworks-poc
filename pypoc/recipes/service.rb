e507618dd097630a743672c65c535603e0a98b6e
Chenyang Liu <chenyang.liu@bodylabs.com>
2015-09-29 22:19:21 -0400

    Revert "Insane"
    This reverts commit 3c0f6693e0603886810f646851c4e1e876be189c.

--- pypoc/recipes/service.rb
+++ pypoc/recipes/service.rb
@@ -9,7 +9,7 @@ supervisor_service 'numpy_app' do
   action :enable
   autostart false
   autorestart 'unexpected'
-  user 'root'
+  user 'deploy'
   stderr_logfile "#{logdir}/pypoc.stderr.log"
   stdout_logfile "#{logdir}/pypoc.stdout.log"
 
