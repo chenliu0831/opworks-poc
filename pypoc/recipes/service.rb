19f760befa223f3d5178d9df017c303f2d2837e5
Chenyang Liu <chenyang.liu@bodylabs.com>
2015-09-30 10:19:34 -0400

    Tryout env

--- pypoc/recipes/service.rb
+++ pypoc/recipes/service.rb
@@ -4,6 +4,8 @@ include_recipe 'supervisor'
 workdir = "#{node['deploy']['pypoc']['deploy_to']}/current"
 logdir = "#{node['pypoc']['supervisor_log_dir']}"
 
+test_length = node['deploy']['pypoc']['environment_variables']['TEST_LENGTH']
+
 supervisor_service 'numpy_app' do
   command "python #{workdir}/app.py"
   action :enable
@@ -13,5 +15,6 @@ supervisor_service 'numpy_app' do
   stderr_logfile "#{logdir}/pypoc.stderr.log"
   stdout_logfile "#{logdir}/pypoc.stdout.log"
 
-  environment "PYTHONUNBUFFERED" => node['pypoc']['BUFFER_TIMEOUT']
+  environment "PYTHONUNBUFFERED" => node['pypoc']['BUFFER_TIMEOUT'],
+              "TEST_LENGTH" => test_length
 end
