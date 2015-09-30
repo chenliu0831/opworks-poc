048124d45951c8c53b13d746b939825bf1130f62
Chenyang Liu <chenyang.liu@bodylabs.com>
2015-09-29 21:38:56 -0400

    use correct user.

--- pypoc/attributes/default.rb
+++ pypoc/attributes/default.rb
@@ -1,7 +1,3 @@
 # Defaults attributes
 default['python']['version'] = '2.7.10'
-
-default['deploy']['pypoc']['user'] = 'bodylabs'
-default['deploy']['pypoc']['group'] = 'bodylabs'
-
 default['pypoc']['BUFFER_TIMEOUT'] = 1
